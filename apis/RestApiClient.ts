db116b7f26f7d9d5cb08771e12b25474abdb317f
hanzochang <hanzo.chan@gmail.com>
2024-10-02 15:49:55 +0900

    feat: comment out template

--- apis/RestApiClient.ts
+++ apis/RestApiClient.ts
@@ -1,10 +1,10 @@
-import { applicationProperties } from '@/constants/applicationProperties'
-import { Configuration, V1Api } from '../generated/rest-api'
+// import { applicationProperties } from '@/constants/applicationProperties'
+// import { Configuration, V1Api } from '../generated/rest-api'
 
-export class RestApiClient {
-  private config = new Configuration({
-    basePath: applicationProperties.API_REST_URL,
-  })
+// export class RestApiClient {
+//   private config = new Configuration({
+//     basePath: applicationProperties.API_REST_URL,
+//   })
 
-  public v1 = new V1Api(this.config)
-}
+//   public v1 = new V1Api(this.config)
+// }
