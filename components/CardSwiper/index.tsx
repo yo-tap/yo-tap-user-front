d20f5afd7399043196f9a669ad0a7271e44d288b
hanzochang <hanzo.chan@gmail.com>
2024-10-03 14:39:57 +0900

    feat: revise

--- components/CardSwiper/index.tsx
+++ components/CardSwiper/index.tsx
@@ -50,14 +50,6 @@ const Component: FC<Props> = ({ reviewableContents, onSwiped }) => {
           </button>
           <button className="swiper-tinder-button swiper-tinder-button-yes">
             <Box className="material-icons-outlined">favorite</Box>
-            {/* <svg
-              xmlns="http://www.w3.org/2000/svg"
-              height="48"
-              viewBox="0 -960 960 960"
-              width="48"
-            >
-              <path d="m480-121-41-37q-106-97-175-167.5t-110-126Q113-507 96.5-552T80-643q0-90 60.5-150.5T290-854q57 0 105.5 27t84.5 78q42-54 89-79.5T670-854q89 0 149.5 60.5T880-643q0 46-16.5 91T806-451.5q-41 55.5-110 126T521-158l-41 37Z" />
-            </svg> */}
           </button>
         </Flex>
       </div>
@@ -73,10 +65,10 @@ type SwiperSlideWrapperProps = {
 
 const SwiperSlideWrapper: FC<SwiperSlideWrapperProps> = ({ imageUri }) => {
   return (
-    <Flex w={458} h={740} pos="relative" justify="center">
+    <Flex w={458} h={620} pos="relative" justify="center">
       <Box
         w={358}
-        h={600}
+        h={500}
         style={{
           backgroundColor: 'white',
           boxShadow: '0px 10px 44px rgba(0,0,0,0.06)!important',
@@ -84,6 +76,16 @@ const SwiperSlideWrapper: FC<SwiperSlideWrapperProps> = ({ imageUri }) => {
           overflow: 'hidden',
         }}
       >
+        <Flex px={24} py={24}>
+          <Box>
+            <Box fz={18} fw={700}>
+              WINK LEGACY
+            </Box>
+            <Box fz={12} fw={700}>
+              TAP GAME
+            </Box>
+          </Box>
+        </Flex>
         <Box pos="relative" display="block" w={358} h={600}>
           <Image
             src={imageUri}
