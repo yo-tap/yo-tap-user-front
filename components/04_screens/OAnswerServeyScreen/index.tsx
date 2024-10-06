0afeefafd17335c4616ee47b5bf41948cf4c5593
hanzochang <hanzo.chan@gmail.com>
2024-10-06 22:02:32 +0900

    refactor:

--- components/04_screens/OAnswerServeyScreen/index.tsx
+++ components/04_screens/OAnswerServeyScreen/index.tsx
@@ -1,19 +1,15 @@
 'use client'
 import { ERippleEffect } from '@/components/01_elements/ERippleEffect'
 import { OAddPointBaloon } from '@/components/02_organisms/OAddPointBaloon'
+import { OAnswerComplete } from '@/components/02_organisms/OAnswerComplete'
 import { OContinuousProgress } from '@/components/02_organisms/OContinuousProgress'
 import { ONavBar } from '@/components/02_organisms/ONavBar'
 import { CardSwiper } from '@/components/CardSwiper'
-import { Box, Flex, Text } from '@mantine/core'
+import { surveyEntityMock } from '@/mocks/surveyEntity.mock'
+import { SurveyEntity } from '@/types/Survey'
+import { Box } from '@mantine/core'
 import { FC, memo } from 'react'
 import { useAnswerServeyScreen } from './hooks'
-import { SurveyEntity } from '@/types/Survey'
-import { surveyEntityMock } from '@/mocks/surveyEntity.mock'
-import { OCounter } from '@/components/02_organisms/OCounter'
-import Image from 'next/image'
-import { fontInterItalic, fontShrikhand } from '@/utils/fonts'
-import { OActionButton } from '@/components/02_organisms/OActionButton'
-import { OAnswerComplete } from '@/components/02_organisms/OAnswerComplete'
 
 type Props = {
   surveyEntity: SurveyEntity
